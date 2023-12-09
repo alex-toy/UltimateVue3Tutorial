54d00073a53bf01010d732682f545cad45c917bb
alex-toy <alexei.80@hotmail.fr>
2023-12-09 08:29:14 +0100

    vue counter app

---
+++ VueCounter2App/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+  export default {
+    data() {
+      return {
+        message: '',
+        clickResult : ''
+      }
+    },
+    methods: {
+      upperMessage () {
+        return this.message.toUpperCase() + "!!!";
+      },
+
+      handleClick(){
+        this.clickResult = this.message + 1;
+      } 
+    }
+  }
+</script>
+
+<template>
+  <main>
+      <container class="container">
+        <h4>the current count is...</h4>
+        <p>Message is: {{ upperMessage() }}</p>
+        <input v-model="message" placeholder="edit me" />
+        <button @click="handleClick()">Click me</button>
+        <h5>You have clicked : {{ clickResult }}</h5>
+      </container>
+  </main>
+</template>
--- VueCounterApp/src/App.vue
+++ VueCounterApp/src/App.vue
@@ -1,47 +1,31 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup>
+  import {ref} from "vue"
+
+  let count = ref(0)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+      <container class="container">
+        <h4>the current count is...</h4>
+        <h1>{{count}}</h1>
+        <button @click="count--">-</button>
+        <button @click="count++">+</button>
+      </container>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+  main {
+    width: 100vw;
+    height: 100vh;
+    background-color: aliceblue;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    align-items: center;
+    justify-content: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .container {
+    text-align: center;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
